6930150f838829249a4a1cea00a2072cfe31db0f
0x1ba <1597881921@qq.com>
2023-08-22 16:01:48 +0800

    import utils

--- 236. 二叉树的最近公共祖先/main.go
+++ 236. 二叉树的最近公共祖先/main.go
@@ -1,5 +1,21 @@
 package main
 
-func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+import "leetcode/utils"
 
+func lowestCommonAncestor(root, p, q *utils.TreeNode) *utils.TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	}
+	if left == nil {
+		return right
+	}
+	return left
 }
